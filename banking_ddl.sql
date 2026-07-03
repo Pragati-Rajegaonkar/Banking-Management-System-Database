cd3d65cd5e6fd0051b8fbe62f4f81b4de58d73e2
Pragati Rajegaonkar <pragatiraj35@gmail.com>
2026-07-03 12:48:12 +0530

    Updated table structures and README diagram

--- banking_ddl.sql
+++ banking_ddl.sql
@@ -1,4 +1,3 @@
--- 1. पुरानी टेबल्स और सीक्वेंसेस को हटाना
 DROP TABLE Transactions CASCADE CONSTRAINTS;
 DROP TABLE Accounts CASCADE CONSTRAINTS;
 DROP TABLE Customers CASCADE CONSTRAINTS;
@@ -6,12 +5,11 @@ DROP SEQUENCE seq_cust_id;
 DROP SEQUENCE seq_acc_no;
 DROP SEQUENCE seq_tx_id;
 
--- 2. सीक्वेंसेस को बिल्कुल शुरुआत से बनाना
 CREATE SEQUENCE seq_cust_id START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_acc_no START WITH 1001 INCREMENT BY 1;
 CREATE SEQUENCE seq_tx_id START WITH 5001 INCREMENT BY 1;
 
--- 3. Customers Table बनाना
+
 CREATE TABLE Customers (
     customer_id NUMBER PRIMARY KEY,
     first_name VARCHAR2(50) NOT NULL,
@@ -20,7 +18,7 @@ CREATE TABLE Customers (
     email VARCHAR2(100) UNIQUE
 );
 
--- 4. Accounts Table बनाना
+
 CREATE TABLE Accounts (
     account_no NUMBER PRIMARY KEY,
     customer_id NUMBER,
@@ -30,7 +28,7 @@ CREATE TABLE Accounts (
     CONSTRAINT fk_cust_id FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- 5. Transactions Table बनाना
+
 CREATE TABLE Transactions (
     transaction_id NUMBER PRIMARY KEY,
     account_no NUMBER,
